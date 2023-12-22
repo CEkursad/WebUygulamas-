2e9a4770ffb5af1a5accdf790f54cba66879a9c8
kursa <kursa@KURSAD>
2023-12-22 22:29:18 +0300

    Add project files.

---
+++ WebUygulaması/Controllers/countriesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUygulaması.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class countriesController : Controller
+    {
+        [HttpGet]
+        public string[] Get()
+        {
+            string[] countries = new string[]
+             { "Almanya", "İngiltere","Fransa","Hırvatistan","Kanada","Macaristan","Bulgaristan","Letonya","Türkiye",
+            "Irak","İran","Ukrayna","Rusya","Azerbaycan","Portekiz","İspanya","Norveç","İsveç","İsviçre","Makedonya"};
+            return countries;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string[] countries = new string[]
+            {
+                  "Almanya", "İngiltere","Fransa","Hırvatistan","Kanada","Macaristan","Bulgaristan","Letonya","Türkiye",
+            "Irak","İran","Ukrayna","Rusya","Azerbaycan","Portekiz","İspanya","Norveç","İsveç","İsviçre","Makedonya"};
+
+            if (id > 0 && id < countries.Length)
+            {
+                return countries[id];
+            }
+            else 
+            
+                return id + " nolu id listede mevcut degildir lutfen 1 ile 20 arasında deger isteyiniz";
+
+            
+        }
+    }
+}
